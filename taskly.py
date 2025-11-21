a08f9c3939ae257966416ff6440c551087b77c63
Ayda Karimzadeh <akarimzade257@gmail.com>
2025-11-21 22:06:59 +0330

    Basic Add & Prioritize Tasks

--- taskly.py
+++ taskly.py
@@ -9,3 +9,31 @@ def load_tasks():
                 if line:
                     tasks.append(line)
     return tasks
+
+user_input = input("Enter your tasks separated by ';':")
+new_tasks = user_input.split(";")
+tasks = load_tasks()
+tasks.extend(new_tasks)
+
+print("\nYour current tasks:")
+for i, task in enumerate(tasks, 1):
+    print(f"{i}) {task}")
+
+priority_input = input("\nDo you want to mark important tasks? (yes/no): ").lower()
+
+if priority_input == "yes":
+    important_tasks = input("Enter the number of important tasks separated by ',':")
+    important_numbers = [int(num) for num in important_tasks.split(",")]
+    
+    if len(important_numbers) > 3:
+        important_numbers = important_numbers[:3]
+        print("You can only mark up to 3 tasks as important.")
+
+    for index in important_numbers:
+     tasks[index - 1] = tasks[index - 1] + "|important"
+
+with open("tasks.txt","w",encoding="utf-8") as file:
+    for task in tasks:
+        file.write(task + "\n")
+
+print("\nTasks saved successfully! 🎀")
